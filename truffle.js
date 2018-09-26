bb0706b37557214dffb9eabd5cb483269e779659
kaushalop <kaushalop7@gmail.com>
2018-09-25 17:08:09 -0700

    Fixed commit to use the Rinkeby network

---
+++ truffle.js
@@ -0,0 +1,13 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '4',
+      gas: 4700000
+    }
+  }
+}
